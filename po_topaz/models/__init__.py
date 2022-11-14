7f426a02f2c8cd8d9a8c2c9308f74ae8e7f8c0b6
AhmedNabilTopaz <60816134+AhmedNabilTopaz@users.noreply.github.com>
2022-11-14 17:39:42 +0200

    po commit

---
+++ po_topaz/models/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+from . import purchase
+# from . import sale_order_line
+# from . import stock_move
+# from . import product_template
+# from . import product
+# from . import stock_quant
+# from . import purchase_report
+# from . import sale_report
+# from . import stock_valuation_layer
+# from . import stock_move_line
+# from . import report_stock_quantity
